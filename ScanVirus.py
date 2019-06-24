9073fee0e75ad8d1590df923c961cd8458b3d922
Penton7 <ontarget1212@gmail.com>
2019-06-24 15:18:54 +0300

    Added send bot info.
    Signed-off-by: Penton7 <ontarget1212@gmail.com>

--- ScanVirus.py
+++ ScanVirus.py
@@ -1,10 +1,8 @@
 import requests
 import telebot
 import config
-import sys
 
 bot = telebot.TeleBot(config.token)
-file = ''
 
 @bot.message_handler(content_types=['document'])
 def handle_docs_photo(message):
@@ -18,33 +16,31 @@ def handle_docs_photo(message):
     with open(src, 'wb') as new_file:
         new_file.write(downloaded_file)
 
-    bot.reply_to(message, "Start scans")
-    url0 = 'https://www.virustotal.com/vtapi/v2/file/scan'
-    params0 = {'apikey': '958b49fa9966ae15cee97686c744b717f3a8a9a8b9f8d93f20f19a312a07991f'}
+    bot.reply_to(message, "Wait pls...")
+    #Send file to virustotal
+    params0 = {'apikey': config.api}
     files = {'file': (str(src), open(str(src), 'rb'))}
-    response = requests.post(url0, files=files, params=params0)
-    #print(response.json())
+    response = requests.post(config.url_scan, files=files, params=params0)
     resource = response.json()["resource"]
 
-    url = 'https://www.virustotal.com/vtapi/v2/file/report'
-    params = {'apikey': '958b49fa9966ae15cee97686c744b717f3a8a9a8b9f8d93f20f19a312a07991f', 'resource': str(resource)}
-    #    print(start.resource)
-    response = requests.get(url, params=params)
+    #Report antivirus
+    params = {'apikey': config.api, 'resource': str(resource)}
+    response = requests.get(config.url_info, params=params)
     scans = response.json()["scans"]
-    sys.stdout = open("/home/penton7/test/text.txt", "w")
-
+    fal = 0
+    tru = 0
     for key, det in scans.items():
-        print("[Antivirus] " + key + " | ", end="")
-        print("Detected:", det['detected'])
-    sys.stdout.close()
-    bot.reply_to(message, open("/home/penton7/test/text.txt", "r"))
+        if det['detected'] == False:
+            fal += 1
+        else:
+            tru += 1
 
+    bot.reply_to(message, 'Detected: ' + str(tru) + "\n" + "Clear: "+ str(fal) + "\n" + "Link: " + "https://www.virustotal.com/gui/file/"+ str(resource) + "/detection"  )
  except Exception as e:
     bot.reply_to(message, e)
 
 bot.polling(none_stop=True, interval=0)
 
-#https://api.telegram.org/bot554264595:AAElo0W0P4QNG3LfGmmnR0TifnavgxDNOeU/getFile?file_id=BQADAgADvQMAAr7LGUi8K-ZIBmvBMQI
 
 
 
--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
 # You token telegram bot
 token = '554264595:AAElo0W0P4QNG3LfGmmnR0TifnavgxDNOeU'
+api = '958b49fa9966ae15cee97686c744b717f3a8a9a8b9f8d93f20f19a312a07991f'
+url_scan = 'https://www.virustotal.com/vtapi/v2/file/scan'
+url_info = 'https://www.virustotal.com/vtapi/v2/file/report'
