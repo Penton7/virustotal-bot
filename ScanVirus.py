7ab8e44e09d512d6734848c73df9900ca7f49f45
Penton7 <ontarget1212@gmail.com>
2019-09-09 17:09:04 +0300

    api-v3/sendfile

--- ScanVirus.py
+++ ScanVirus.py
@@ -1,48 +1,45 @@
 import requests
 import telebot
 import config
+import time
+
 
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=['document'])
 def handle_docs_photo(message):
  try:
+
     chat_id = message.chat.id
 
     file_info = bot.get_file(message.document.file_id)
     downloaded_file = bot.download_file(file_info.file_path)
-
-    src = '/home/penton7/test/' + message.document.file_name;
+   
+    src = '/home/penton7/projects/test/' + message.document.file_name
     with open(src, 'wb') as new_file:
         new_file.write(downloaded_file)
 
     bot.reply_to(message, "Wait pls...")
     #Send file to virustotal
-    params0 = {'apikey': config.api}
-    files = {'file': (str(src), open(str(src), 'rb'))}
-    response = requests.post(config.url_scan, files=files, params=params0)
-    resource = response.json()["resource"]
+    headers = {'x-apikey':'958b49fa9966ae15cee97686c744b717f3a8a9a8b9f8d93f20f19a312a07991f'}
+    files = {'file': (src)+ message.document.file_name}
+    print(files)
+    response = requests.post("https://www.virustotal.com/api/v3/files", headers=headers, files=files )
+    print(response)
+    resource = response.json()["data"]["id"]
 
     #Report antivirus
     params = {'apikey': config.api, 'resource': str(resource)}
-    response = requests.get(config.url_info, params=params)
-    scans = response.json()["scans"]
-    fal = 0
-    tru = 0
-    for key, det in scans.items():
-        if det['detected'] == False:
-            fal += 1
-        else:
-            tru += 1
-
-    bot.reply_to(message, 'Detected: ' + str(tru) + "\n" + "Clear: "+ str(fal) + "\n" + "Link: " + "https://www.virustotal.com/gui/file/"+ str(resource) + "/detection"  )
+    response = requests.get("https://www.virustotal.com/api/v3/analyses/NTZiNDg5OWU4NDQ2MjlkNDJjZmNlN2Y4Nzc4OTM4MmE6MTU2ODAzMDY0OQ==", headers=headers)
+    time.sleep(30)
+    scans = response.json()["data"]["attributes"]["results"]
+    #tru = 0
+    #for key, det in scans.items():
+     #   if det['detected'] == False:
+      #      bot.reply_to(message, det[''] + 'Virus: ')
+    #bot.reply_to(message, 'Virus: ' + str(tru) + "\n" + "Clear: "+ str(fal) + "\n" + "Link: " + "https://www.virustotal.com/gui/file/" + str(resource) + "/detection")
+    bot.reply_to(message, scans)
  except Exception as e:
     bot.reply_to(message, e)
 
 bot.polling(none_stop=True, interval=0)
-
-
-
-
-
-
