c4cb5ba9b7e84a700972da8eb16863ed2003eaa9
Penton7 <ontarget1212@gmail.com>
2019-09-09 17:21:29 +0300

    restructuring

--- ScanVirus.py
+++ ScanVirus.py
@@ -1,25 +1,26 @@
 import requests
 import telebot
-import config
 import time
 
+token = '814437389:AAHtwOCGU3Zp6s0aMna9w0rXszTUMXvfyb0' #Telegram token
+api = 'x-apikey: 958b49fa9966ae15cee97686c744b717f3a8a9a8b9f8d93f20f19a312a07991f' #VirusTotal api token
+url_scan = 'https://www.virustotal.com/api/v3/files' #Url for scans file and return file id
+url_info = 'https://www.virustotal.com/api/v3/analyses/' #Url for information file
 
-bot = telebot.TeleBot(config.token)
+bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=['document'])
 def handle_docs_photo(message):
  try:
-
+    #Telegram download file
     chat_id = message.chat.id
-
     file_info = bot.get_file(message.document.file_id)
     downloaded_file = bot.download_file(file_info.file_path)
-   
     src = '/home/penton7/projects/test/' + message.document.file_name
     with open(src, 'wb') as new_file:
         new_file.write(downloaded_file)
-
     bot.reply_to(message, "Wait pls...")
+
     #Send file to virustotal
     headers = {'x-apikey':'958b49fa9966ae15cee97686c744b717f3a8a9a8b9f8d93f20f19a312a07991f'}
     files = {'file': (src)+ message.document.file_name}
@@ -28,8 +29,7 @@ def handle_docs_photo(message):
     print(response)
     resource = response.json()["data"]["id"]
 
-    #Report antivirus
-    params = {'apikey': config.api, 'resource': str(resource)}
+    #Report antivirus   
     response = requests.get("https://www.virustotal.com/api/v3/analyses/NTZiNDg5OWU4NDQ2MjlkNDJjZmNlN2Y4Nzc4OTM4MmE6MTU2ODAzMDY0OQ==", headers=headers)
     time.sleep(30)
     scans = response.json()["data"]["attributes"]["results"]
--- config.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# You token telegram bot
-token = '#'
-api = '958b49fa9966ae15cee97686c744b717f3a8a9a8b9f8d93f20f19a312a07991f'
-url_scan = 'https://www.virustotal.com/vtapi/v2/file/scan'
-url_info = 'https://www.virustotal.com/vtapi/v2/file/report'
